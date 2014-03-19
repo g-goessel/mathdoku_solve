f8d83130e625a738400e44e659ea9f80562d4f85
g-goessel <g_goessel@outlook.com>
2014-03-19 19:50:11 +0100

    Redaction des premiers principes, creation de la fonction "clean"

---
+++ fonctions.py
@@ -0,0 +1,23 @@
+"""
+fonctions 
+"""
+
+def clean(x, y ,val,to_clean):
+	"""
+	Cette fonction va nettoyer le tableau "to_clean" des combinaisons impossibles
+	sur les lignes et les colonnes a partir d'une valeur considérée comme certaine "val"
+	située aux coordonnées (x,y)
+	"""
+	for ligne in range(len(to_clean)):
+		#nettoyage de la ligne
+		if ligne!=x:
+			for i in range(9):
+				if to_clean[ligne,y,i]==val: 
+					to_clean[ligne,y,i]=0
+
+	for colonne in range(len(to_clean)):
+		#nettoyage de la colonne
+		if colonne!=y:
+			for i in range(9):
+				if to_clean[x,colonne,i]==val:
+					to_clean[x,colonne,i]=0
