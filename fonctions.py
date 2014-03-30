c13bf48d05fefd5ac4914ea92ace16ffa698cc14
g.goessel <g_goessel@outlook.com>
2014-03-30 18:18:22 +0200

    Suppression d'une fonction inutile N'était pas utilisée et ne correspondait plus au format de stockage choisi

--- fonctions.py
+++ fonctions.py
@@ -5,28 +5,6 @@ fonctions
 from itertools import product
 from functools import reduce
 
-def clean(x, y, val, to_clean):
-    """
-    ACTUELLEMENT INUTILE
-
-    Cette fonction va nettoyer le tableau "to_clean" des combinaisons impossibles
-    sur les lignes et les colonnes a partir d'une valeur considérée comme certaine "val"
-    située aux coordonnées (x,y)
-    """
-    for ligne in range(len(to_clean)):
-        # nettoyage de la ligne
-        if ligne != x:
-            for i in range(9):
-                if to_clean[ligne, y, i] == val:
-                    to_clean[ligne, y, i] = 0
-
-    for colonne in range(len(to_clean)):
-        # nettoyage de la colonne
-        if colonne != y:
-            for i in range(9):
-                if to_clean[x, colonne, i] == val:
-                    to_clean[x, colonne, i] = 0
-
 
 def combi_possibles(val_tot,nbr_cases):
     """
