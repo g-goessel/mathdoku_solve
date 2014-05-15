ed7a03af2503e2bca8850da59eb52e500e39498e
g.goessel <g_goessel@outlook.com>
2014-05-15 22:58:55 +0200

    Still not working ><

--- bruteforce.py
+++ bruteforce.py
@@ -15,23 +15,27 @@ def bruteforce(user_data,size):
     nbr_ite=0
 
     #classement des blocs bar ordre décroissant de combinaisons possibles
-    sorted_blocs=sorted(user_data, key=lambda x : len(user_data[x][2]))
-    print('sorted : ',sorted_blocs)
+    sorted_blocs=sorted(user_data, key=lambda x : len(user_data[x][2]),reverse=True)
 
     #compteur des itérations de la forme {1:[ite en cours, nbr max d'ite], 2: ...}
-    compteur={x:[0,len(user_data[i][2])-1] for x,i in enumerate(sorted_blocs)}
+    compteur=[[0,len(user_data[i][2])-1] for x,i in enumerate(sorted_blocs)]
     #la variable scope va faire référence à la position du bloc dans le compteur où nous sommes actuellement
     scope=0
 
-    to_test = np.zeros((size,size), dtype=np.int8)
+    nbr_blocs=len(user_data)
 
-    while scope <= len(user_data):
+    to_test = np.zeros((size,size))
+
+    while 1:
         bloc_de_test=user_data[sorted_blocs[scope]]
         #print(bloc_de_test)
         test=test_insertion(bloc_de_test[1],bloc_de_test[2][compteur[scope][0]],np.copy(to_test),size)
         #print(test)
 
-        if test:
+        if test and scope==nbr_blocs-1:
+            return True,test[1]
+            break
+        elif test:
             scope+=1
             to_test=test[1]
         else:
